5a5ce1186fa481551521141480b1b4e6998a3de0
AdanPradana <adanrizkahariz@gmail.com>
2023-07-19 21:21:01 +0700

    add logic user history

--- Routes/index.js
+++ Routes/index.js
@@ -1,11 +1,13 @@
 const login = require("../Controllers/Login.controller.js");
 const GameRouter = require("./game.js");
 const PlayerRouter = require("./player.js");
+const History = require("./history.js");
 const app = require("express");
 const router = app.Router();
 
 router.use("/players", PlayerRouter);
 router.use("/games", GameRouter);
+router.use("/history", History);
 
 // login handler
 router.post("/auth/login", login);
